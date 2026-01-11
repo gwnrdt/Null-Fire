8f704934047e85fd52812af09c2bf74bf5384c29
gwnrdt <jomardelacruz1882@gmail.com>
2026-01-12 01:35:02 +0800

    Update TurretBypass.lua

--- Core/Loaders/Dead-Rails/TurretBypass.lua
+++ Core/Loaders/Dead-Rails/TurretBypass.lua
@@ -7,7 +7,7 @@ if getGlobalTable().TACB then
 end
 
 local network = loadstring(game:HttpGet("https://raw.githubusercontent.com/gwnrdt/Null-Fire/refs/heads/main/Core/Libraries/Network/Main.lua"))()
-local dragFuncs = loadstring(game:HttpGet("https://raw.githubusercontent.com/InfernusScripts/Null-Fire/refs/heads/main/Core/Loaders/Dead-Rails/DragFunctions.lua", true))()
+local dragFuncs = loadstring(game:HttpGet("https://raw.githubusercontent.com/gwnrdt/Null-Fire/refs/heads/main/Core/Loaders/Dead-Rails/DragFunctions.lua", true))()
 
 local plr = game:GetService("Players").LocalPlayer
 
